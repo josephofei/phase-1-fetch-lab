e53380ff3e7412d0ef1ece1722c6be21ba324aae
Joseph Ofei <josephofei24@gmail.com>
2023-04-13 10:12:36 -0400

    fetch complete

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+    .then(res => res.json())
+    .then(books => renderBooks(books))
+
+  return fetch('https://anapioficeandfire.com/api/books')
+
 }
 
 function renderBooks(books) {
@@ -12,6 +17,6 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   fetchBooks();
 });
